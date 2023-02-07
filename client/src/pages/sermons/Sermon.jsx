34c8b1de9e4380858f06ca4874346dc057d196fe
unknown <agyeis6000@gmail.com>
2023-02-07 17:27:50 +0000

    update

--- client/src/pages/sermons/Sermon.jsx
+++ client/src/pages/sermons/Sermon.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./sermons.css";
-import Slide from "../../components/msgSlider/Slider";
+import Slide from "../../components/msgSlider/slider";
 // import { useLoaderData, useParams } from "react-router-dom";
 
 export default function Sermon() {
@@ -24,9 +24,7 @@ export default function Sermon() {
         </div>
       </section>
       <section id="sermon_text">
-        <div className="sermon_text_wrapper">
-          <h2>THE MOUNTAIN OF EDUCATION</h2>
-        </div>
+        <div className="sermon_text_wrapper"></div>
       </section>
       <Slide />
       {/* sideData={slideData.filter((slideData) => slideData.names != "Jack Ryne")} */}
