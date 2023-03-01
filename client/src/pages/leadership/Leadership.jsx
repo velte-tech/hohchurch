8c16d779b2a34aea6037126425a0d7ba47549fe3
montana30 <tzed30@gmail.com>
2023-03-01 10:30:50 +0000

    link removed from leadership page

--- client/src/pages/leadership/Leadership.jsx
+++ client/src/pages/leadership/Leadership.jsx
@@ -1,5 +1,5 @@
 import { React } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import "./Leadership.css";
 // Images
 import l1 from "../../assets/images/pastor-alex.jpg";
