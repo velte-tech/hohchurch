86cfb46194d65d7849dfa785407e02dccb3fe4d8
DanielGraham123 <dg.danielboaz@yahoo.com>
2023-08-03 17:40:36 +0100

    sermon page integration with dashboard

--- src/pages/sermons/Sermon.jsx
+++ src/pages/sermons/Sermon.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./sermons.css";
 import Slide from "../../components/msgSlider/slider";
 import { useParams } from "react-router-dom";
@@ -8,16 +8,18 @@ import sanityClient from "../../client";
 
 export default function Sermon() {
   const { id } = useParams();
-  const item = slideData.find((item) => item.id === id);
+  // const item = slideData.find((item) => item.id === id);
+  const [item, setItem] = useState({});
 
   useEffect(() => {
     sanityClient
       .fetch(`*[_type == "slider" && _id == "${id}"]{..., image->}`)
       .then((data) => {
         console.log("slider: ", data);
+        setItem(data[0]);
       })
       .catch(console.error);
-  }, []);
+  }, [id]);
 
   return (
     <div>
@@ -27,7 +29,7 @@ export default function Sermon() {
             id="utube_video"
             width="760"
             height="415"
-            src={item.utube}
+            src={item.link}
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
