9710a2bc7fb231be9648ee732a87eb3e5597e600
DanielGraham123 <dg.danielboaz@yahoo.com>
2023-08-10 15:36:16 +0100

    outreach details page integration 97%

--- src/utils/contentSerializers.js
+++ src/utils/contentSerializers.js
@@ -1,13 +1,30 @@
 export const serializers = {
     types: {
         block: (props) => {
-            const { style } = props.node;
+            const { style, markDefs } = props.node;
+            console.log("markDefs", markDefs);
+
             if (style === "normal") {
+                if (markDefs.length !== 0) {
+                    return (
+                        <>{props.children}</>
+                    )
+                }
                 if (props.children.length === 1 && props.children[0] === "") {
                     return <br />;
                 }
                 return <p>{props.children}</p>;
             }
+
         },
+        link: ({ mark, children }) => {
+            const { href } = mark;
+            console.log("mark", children);
+            return (
+                <a href={href} target="_blank" style={{ color: 'red !important' }}>
+                    {children}
+                </a>
+            );
+        }
     },
 };
